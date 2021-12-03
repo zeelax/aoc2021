24404a12573e8340643a106bcd6b57a186a2c19a
Maxim Kravets <zeelax@gmail.com>
2021-12-03 19:55:08 +0100

    Halt and catch fire

--- 03/2.sh
+++ 03/2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 calculate_gamma_epsilon() {
     awk "/^$1/" input.txt | awk 'NR=1 {len = length($1)} {for (i = 1; i <= len; ++i) bits[i] = bits[i]+substr($1,i,1)} END {for (i=1; i<=len; ++i) {if (bits[i] >= FNR/2) {gamma = gamma 1; epsilon = epsilon 0} else {gamma = gamma 0; epsilon = epsilon 1}} print gamma, epsilon, FNR}'
 }
