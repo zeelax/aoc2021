0cc6e51569846131123115ddea4d040c8bcb05c6
Maxim Kravets <zeelax@gmail.com>
2021-12-03 19:35:36 +0100

    day 3

--- 03/1.sh
+++ 03/1.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-awk '/forward/ {h=h+$2} /down/ {d=d+$2} /up/ {d=d-$2} END {print h*d}' input.txt
+awk 'NR=1 {len = length($1)} {for (i = 1; i <= len; ++i) bits[i] = bits[i]+substr($1,i,1)} END {for (i=1; i<=len; ++i) {if (bits[i] > FNR/2) {gamma = gamma 1; epsilon = epsilon 0} else {gamma = gamma 0; epsilon = epsilon 1}} printf "ibase=2;%s*%s\n", gamma, epsilon}' input.txt | bc
--- 03/2.sh
+++ 03/2.sh
@@ -1,3 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
-awk '/forward/ {h=h+$2; d=d+a*$2} /down/ {a=a+$2} /up/ {a=a-$2} END {print h*d}' input.txt
+calculate_gamma_epsilon() {
+    awk "/^$1/" input.txt | awk 'NR=1 {len = length($1)} {for (i = 1; i <= len; ++i) bits[i] = bits[i]+substr($1,i,1)} END {for (i=1; i<=len; ++i) {if (bits[i] >= FNR/2) {gamma = gamma 1; epsilon = epsilon 0} else {gamma = gamma 0; epsilon = epsilon 1}} print gamma, epsilon, FNR}'
+}
+
+#calculate co2 scrubber rating
+position=1
+regex=''
+while true; do
+    output=($(gamma_epsilon "$regex"))
+    gamma=${output[0]}
+    epsilon=${output[1]}
+    numbers_left=${output[2]}
+    regex="${regex}${gamma:$position-1:1}"
+    ((position++))
+    if [ $numbers_left == "1" ];
+    then
+        break;
+    fi
+done
+oxygen=$gamma
+
+#calculate co2 scrubber rating
+position=1
+regex=''
+while true; do
+    output=($(gamma_epsilon "$regex"))
+    gamma=${output[0]}
+    epsilon=${output[1]}
+    numbers_left=${output[2]}
+    regex="${regex}${epsilon:$position-1:1}"
+    ((position++))
+    if [ $numbers_left == "1" ];
+    then
+    	break;
+    fi
+done
+co2=$gamma
+
+echo -e "ibase=2;$oxygen*$co2\n" | bc
